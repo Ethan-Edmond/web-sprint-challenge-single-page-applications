4e8f3566be9241f3eb1e08c62ea60d721a3ccd01
Ethan-Edmond <eedmond@uccs.edu>
2021-05-21 07:08:41 -0600

    Installed cypress and added imports to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
-    </>
+    </Router>
   );
 };
 export default App;
